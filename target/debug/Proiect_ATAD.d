5ae3a626be72f99553f589d2fb39592321f948c7
iasmina012 <iasmina.putina012@yahoo.com>
2025-11-11 10:42:42 +0200

    Moved html to a new file (index.html)

--- target/debug/Proiect_ATAD.d
+++ target/debug/Proiect_ATAD.d
@@ -1,1 +1,1 @@
-/Users/iasmina012/Desktop/Proiect_ATAD/target/debug/Proiect_ATAD: /Users/iasmina012/Desktop/Proiect_ATAD/src/main.rs
+/Users/iasmina012/Desktop/Proiect_ATAD/target/debug/Proiect_ATAD: /Users/iasmina012/Desktop/Proiect_ATAD/src/index.html /Users/iasmina012/Desktop/Proiect_ATAD/src/main.rs
